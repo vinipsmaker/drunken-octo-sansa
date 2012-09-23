ab247969f237984c061e8e9e9ffa3ac93a1829b9
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2012-09-22 23:36:02 -0300

    Solution (WIP) for uva problem 100

---
+++ 100.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+long int f(long int n)
+{
+    long int blueoyster = 1;
+
+    while (n != 1){
+        if (n % 2 != 0)
+            n = 3 * n + 1;
+        else
+            n = n / 2;
+
+        blueoyster++;
+    }
+    return blueoyster;
+}
+int main ()
+{
+    long int i,j;
+
+    while (cin >> i >> j){
+        if (i > j) {
+            long int aux = i;
+            i = j;
+            j = aux;
+        }
+        long int max = 0;
+        cout << i << ' ' << j
+             << ' ';
+        while (i <= j){
+            long int aux = f(i);
+            if (aux > max)
+                max = aux;
+                
+            i++;
+        }
+        cout << max << '\n';
+    }
+}
